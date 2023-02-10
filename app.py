8bbd75930a94c4294d7d35c99a15aa84168d32c0
fawwazanvilen <fawwazanvi@gmail.com>
2023-02-10 23:41:27 +0700

    trying out small pickles

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import openai
 from openai.embeddings_utils import get_embedding, cosine_similarity
 
-dir = 'data_merged_all_01.pickle' #change this to relevant source
+# dir = 'data_merged_all_01.pickle' #change this to relevant source
+dir = 'data_merged_test_small.pickle'
 
 with open(dir, 'rb') as file:
     # Deserialize the DataFrame using pickle.load()
@@ -38,7 +39,7 @@ prompt = st.text_input('Masukan prompt di sini')
 if st.button("Cari!"):
     search = search_reviews(data_merged_all, prompt, n=20)
     
-    Data = {'UU'      : search['pdf_names'].values[:20],   #change p4ompt to 20
+    Data = {'UU'      : search['pdf_names'].values[:20],   #change prompt to 20
             'Tentang' : search['tentang'].values[:20],     
             'Halaman' : search['page_number'].values[:20]+1}
         
